c61fd11038ba8890221208cdaff5f4b05369050b
Sherry Li <sherryli@Sherrys-MacBook-Pro.local>
2017-12-03 23:57:51 +1100

    done

--- Sherry/wk01/4-thu/ptplanner.js
+++ Sherry/wk01/4-thu/ptplanner.js
@@ -1,54 +1,82 @@
+// origin: Melbourne Central
+// destination: Richmond
+// Melbourne Central -----> Parliament -----> Richmond
+// 2 stops total
+
 console.log("PT PLANNER");
+var linesOptions = ["alamein", "glenWaverly", "sandringham"];
+/*var choosenDesLine = prompt("chose a line for your destination from alamein, glenWaverly, sandringham.");
+
+if (linesOptions.indexOf(choosenDesLine) !== -1) { // make sure the line user chose is from one of 3 lines
+	console.log("your destination line is " + choosenDesLine + " line");
+} else {
+	prompt("please try again.");
+}
+*/
 var origin = "Flagstaff";
 console.log("origin: " + origin);
 var destination = "Burnley";
 console.log("destination: " + destination); 
-var choosenLine = "glenWaverly";
+// var choosenDesLine = "glenWaverly";
 
 
-var alamein = ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"];
-var glenWaverly = ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong", "Tooronga"];
-//var sandringham = [Southern Cross, Richmond, South Yarra, Prahran, and Windsor]
+var alamein = new Array ("Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie");
+var glenWaverly =  new Array ("Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong", "Tooronga");
+var sandringham = new Array ("Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor");
+var lines = new Array (alamein, glenWaverly, sandringham);
+//  var i = "";  //not sure for it
 
-if (choosenLine === "glenWaverly") {   // user's input is glenWaverly
-   if (glenWaverly.indexOf(destination) === -1) {  // to check whether "Burnley" is in glenWaverly array, if it's not,
-		var indexDesTwo = alamein.indexOf(destination);  // to find out alamein's index for destination to calulate totalstops cause only 2 lines added in for now, so "B" must be in alamein array
-		var indexDesTransfer = glenWaverly.indexOf(alamein[1]); // "Rich" is the destination for glen line.
-		var indexOriTransfer = alamein.indexOf(alamein[1]);   // "Rich" is the origin for alamein
-	} 
-	else {
-   	 	var indexDesTwo = glenWaverly.indexOf(destination);
-   	}
+var lineOri = "";
+var lineDes = "";
+var userJourney="";
+
+var indexOri = -1;
+var indexRichOri = -1;
+
+var indexDes = -1;
+var indexRichDes = -1;
+
+for (var i = 0; i < lines.length; i++ ) {
+  //for (var j = 0; j < lines[i].length; j++ ) {
+  	if(lines[i].indexOf(origin) != -1) {//find out the origin line  		
+  		
+  		indexOri = lines[i].indexOf(origin);
+  		indexRichOri = lines[i].indexOf("Richmond"); // "Rich" is the destination for the origin line
+  		if(indexOri > indexRichOri) {
+   			lines[i] = lines[i].reverse();
+   		}
+  		for (var a = indexOri; a <= indexRichOri; a++) { 
+			if(lineOri) {// if lineOri is not empty, then concat string
+				lineOri = lineOri + " -----> " + lines[i][a];
+			} else {// if it is empty, then assign lineOri to lines[i][a]
+				lineOri = lines[i][a];
+			}		
+		}  
+  		
+  	}
+  	if(lines[i].indexOf(destination) !== -1) { // find out the destination line
+  		indexDes = lines[i].indexOf(destination);
+  		indexRichDes = lines[i].indexOf("Richmond"); // "Rich" if the first stop for the line of indexDes
+  		
+  		if(indexRichDes > indexDes) {
+  			lines[i] = lines[i].reverse();
+  		}
+
+  		for (var b = indexRichDes + 1; b <= indexDes; b++) { //b= indexOf richmont, it would be running twice when excute this loop, so need to jump over it			
+  			lineDes = lineDes + " -----> " + lines[i][b]; 				
+  		}
+  		
+  	}    
+  //}
 }
+userJourney = lineOri + lineDes;
 
+//console.log(indexOri, indexDes);
+//console.log(indexRichOri, indexRichDes);
+console.log(userJourney);
 
-
-
-// from js;27 the code only run for the case with 1 trainline added in
-var indexOri = alamein.indexOf(origin); 
-var indexDes = alamein.indexOf(destination);
-if (indexOri > indexDes) {
-   alamein = alamein.reverse();
-   indexOri = alamein.indexOf(origin);
-   indexDes = alamein.indexOf(destination);
-} 
-
-var lineX = "";
-for (var i = indexOri; i <= indexDes; i++) {
-	//Stop 1
-	//if (!lineX) {
-	//}
-	//Stop 2
-	//Stop 3
-  	//console.log(i);
-  	lineX = lineX + " -----> " + alamein[i]; 
-     //lineX = lineX + alamein[i];  	 	
-}
-console.log(lineX);
-
-var totalstops = Math.abs(indexOri - indexDes);
+var totalstops = Math.abs((indexOri - indexRichOri) + (indexRichDes - indexDes));
 console.log(totalstops + " stops total");
-
 
 
 
